590b0158dc18bfe0d55d98c3f322d046eb15db5f
jbecker <julix.beker@gmail.com>
2022-11-23 00:23:53 +0100

    Add ExpansesInputService

---
+++ src/expanses/ExpansesInputService.java
@@ -0,0 +1,22 @@
+package expanses;
+
+import validation.ValidationService;
+
+public class ExpansesInputService {
+    private final ValidationService validationService = new ValidationService();
+    final private String[] CATEGORIES = {"Food", "Transport", "Shopping", "Other"};
+
+    public int[][] getExpanses() {
+        int days = validationService.validateInputIsInt("Enter number of days");
+        int[][] expanses = new int[days][CATEGORIES.length];
+        for (int i = 0; i < days; i++) {
+            for (int j = 0; j < CATEGORIES.length; j++) {
+                expanses[i][j] = validationService.validateInputIsInt("Enter expanses for " + CATEGORIES[j] + " for day " + (i + 1));
+            }
+        }
+        return expanses;
+    }
+
+
+
+}
