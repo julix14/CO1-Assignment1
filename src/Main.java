1711010b04a5bbb1a07eb1e8e53f674ab8127b2f
jbecker <julix.beker@gmail.com>
2022-11-23 18:38:40 +0100

    Add methods to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import expanses.ExpanseOutputService;
+import expanses.ExpansesInputService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ExpansesInputService expansesInputService = new ExpansesInputService();
+
+        int budget = expansesInputService.getBudget();
+        int[][] expanses = expansesInputService.getExpanses();
+
+        ExpanseOutputService expanseOutputService = new ExpanseOutputService(expanses, budget);
+
+        expanseOutputService.selectAction();
+
     }
 }
